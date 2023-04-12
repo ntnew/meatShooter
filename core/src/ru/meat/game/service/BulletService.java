f8c73fe9881251a0d556289e1a73e298a12e281a
Ahab <ntnew@yandex.ru>
2023-04-12 23:25:28 +0400

    рефактор взрывов

--- core/src/ru/meat/game/service/BulletService.java
+++ core/src/ru/meat/game/service/BulletService.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
